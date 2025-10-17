9542606dbe0c1a5f2eb926c9839b99bfbeb7252e
thilina98 <thilina.avishka@gmail.com>
2025-10-17 08:35:17 +0530

    local init

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,5 @@
+import 'dotenv/config'
+
 import express, { type Request, Response, NextFunction } from "express";
 import session from "express-session";
 import connectPgSimple from "connect-pg-simple";
